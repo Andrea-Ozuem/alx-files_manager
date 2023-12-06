6881c5c99b59acf358a3bf4efee386c2803bbacd
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-12-06 16:25:45 +0100

    Redis setup

---
+++ utils/redis.js
@@ -0,0 +1,32 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (err) => console.log(err.message));
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const asyncGet = promisify(this.client.GET).bind(this.client);
+    const val = await asyncGet(key);
+    return val;
+  }
+
+  async set(key, val, time) {
+    const asyncSet = promisify(this.client.SET).bind(this.client);
+    await asyncSet(key, val, 'EX', time);
+  }
+
+  async del(key) {
+    const delAsync = promisify(this.myClient.DEL).bind(this.myClient);
+    await delAsync(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
